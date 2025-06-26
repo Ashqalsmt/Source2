206762f96a73bb4867a509aa17b42acc6c14adcc
Ashqalsmt <128143959+Ashqalsmt@users.noreply.github.com>
2025-06-26 18:40:19 +0300

    Update zq_lo_config.py

--- yamenthon/Config/zq_lo_config.py
+++ yamenthon/Config/zq_lo_config.py
@@ -2,11 +2,9 @@
 
 import os
 
-ENV = bool(os.environ.get("ENV", "ANYTHING"))
+ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
     from sample_config import Config  # noqa
 elif os.path.exists("config.py"):
     from config import Development as Config  # noqa
-else:
-    from sample_config import Config  # noqa
